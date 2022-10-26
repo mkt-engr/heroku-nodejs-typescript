82a985cac225607365be0cb31430d9e5acc34435
森 真輝人 <mkt.it.engr@gmail.com>
2022-10-26 19:16:18 +0900

    リファクタ

--- index.ts
+++ index.ts
@@ -1,12 +1,11 @@
-import express from 'express';
-import path from 'path';
+import express, { Request, Response } from "express";
+import path from "path";
 
-const PORT = process.env.PORT || 5000;
-const server = express();
-
-express()
-  .use(express.static(path.join(__dirname, '../public')))
-  .set('views', path.join(__dirname, '../views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+const PORT = process.env.PORT || 5001;
+const app = express();
+app
+  .use(express.static(path.join(__dirname, "../public")))
+  .set("views", path.join(__dirname, "../views"))
+  .set("view engine", "ejs")
+  .get("/", (req: Request, res: Response) => res.render("pages/index"))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
