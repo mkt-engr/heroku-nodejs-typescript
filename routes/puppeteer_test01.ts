c10f949c60686bb2a7f4ca1b3d943bbc97e69f53
森 真輝人 <mkt.it.engr@gmail.com>
2022-10-26 19:39:01 +0900

    install.jsを実行

--- routes/puppeteer_test01.ts
+++ routes/puppeteer_test01.ts
@@ -8,32 +8,38 @@ router.get("/puptest", async (req: Request, res: Response) => {
   const LAUNCH_OPTION = process.env.DYNO
     ? { args: ["--no-sandbox", "--disable-setuid-sandbox"] }
     : { headless: false };
-  // const browser = await puppeteer.launch(LAUNCH_OPTION);
-  const browser = await puppeteer.launch({
-    args: ["--no-sandbox", "--disable-setuid-sandbox"],
-  });
-  const page = await browser.newPage();
-  // Googleページを開く
-  await page.goto("https://www.google.com/");
-  // 検索boxに`puppeteer`を入力
-  await page.type('input[name="q"]', "puppeteer");
-  // 「Enter」ボタン押下
-  await page.keyboard.press("Enter");
-  // 検索結果要素の表示まで待機
-  await page.waitForSelector(".LC20lb", { visible: true });
-  // 検索結果のタイトル・リンク一覧取得
-  const searchResults = await page.evaluate(() =>
-    [...document.querySelectorAll<HTMLAnchorElement>(".LC20lb")].map(
-      (element) => {
-        const ppp = element.parentElement as HTMLAnchorElement;
-        return {
-          link: element.href || ppp.href || "何もなかった",
-          title: element.innerText,
-        };
-      }
-    )
-  );
-  console.log(searchResults);
+  let searchResults;
+  try {
+    // const browser = await puppeteer.launch(LAUNCH_OPTION);
+    const browser = await puppeteer.launch({
+      args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    });
+    const page = await browser.newPage();
+    // Googleページを開く
+    await page.goto("https://www.google.com/");
+    // 検索boxに`puppeteer`を入力
+    await page.type('input[name="q"]', "puppeteer");
+    // 「Enter」ボタン押下
+    await page.keyboard.press("Enter");
+    // 検索結果要素の表示まで待機
+    await page.waitForSelector(".LC20lb", { visible: true });
+    // 検索結果のタイトル・リンク一覧取得
+    searchResults = await page.evaluate(() =>
+      [...document.querySelectorAll<HTMLAnchorElement>(".LC20lb")].map(
+        (element) => {
+          const ppp = element.parentElement as HTMLAnchorElement;
+          return {
+            link: element.href || ppp.href || "何もなかった",
+            title: element.innerText,
+          };
+        }
+      )
+    );
+    console.log(searchResults);
+  } catch (error) {
+    console.error(error);
+  }
+
   // await browser.close();
   res.status(200).send({
     message: searchResults,
