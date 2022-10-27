6e683f9a7b55b10919255d5f7b85185685801bca
森 真輝人 <mkt.it.engr@gmail.com>
2022-10-27 12:08:19 +0900

    ローカルか本番かでchacheDirectoryを変更する

--- .puppeteerrc.cjs
+++ .puppeteerrc.cjs
@@ -1,8 +1,11 @@
-// const { join } = require("path");
-
-// /**
-//  * @type {import("puppeteer").Configuration}
-//  */
-// module.exports = {
-//   cacheDirectory: join(__dirname, ".cache", "puppeteer"),
-// };
+const { join } = require("path");
+const os = require("os");
+/**
+ * @type {import("puppeteer").Configuration}
+ */
+module.exports = {
+  // cacheDirectory: join(__dirname, ".cache", "puppeteer"),
+  cacheDirectory: process.env.MORI
+    ? join(os.homedir(), ".cache", "puppeteer")
+    : join(__dirname, ".cache", "puppeteer"),
+};
--- index.ts
+++ index.ts
@@ -1,3 +1,5 @@
+import * as dotenv from "dotenv"; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+dotenv.config();
 import express, { Request, Response } from "express";
 import path from "path";
 import { puppeteerTest01Router } from "./routes/puppeteer_test01";
@@ -9,7 +11,10 @@ app
   .use(express.static(path.join(__dirname, "../public")))
   .set("views", path.join(__dirname, "../views"))
   .set("view engine", "ejs")
-  .get("/", (req: Request, res: Response) => res.render("pages/index"))
+  .get("/", (req: Request, res: Response) => {
+    console.log(process.env.MORI, "環境変数を読み込みたい");
+    res.render("pages/index");
+  })
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 app.use(puppeteerTest01Router);
